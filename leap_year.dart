1994e612b151ddef0e570f321b54859018a8185b
Paul Botchwey <botchweypaul0001@gmail.com>
2025-08-05 05:47:51 +0000

    Add leap year calculation

---
+++ leap_year.dart
@@ -0,0 +1,12 @@
+void main() {
+  int year = 2020;
+  print("Year $year is: ${isLeapYear(year)}");
+}
+
+String isLeapYear(int year) {
+  if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) {
+    return "Leap Year";
+  } else {
+    return "Not a Leap Year";
+  }
+}
